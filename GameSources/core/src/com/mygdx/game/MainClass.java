dad802e27edc50de2f1a4d65c39fb66623541943
Arcraith <tobias.schweisfurth@web.de>
2015-07-30 12:17:11 +0200

    add erste Sprite Bewegungen

--- GameSources/core/src/com/mygdx/game/MainClass.java
+++ GameSources/core/src/com/mygdx/game/MainClass.java
@@ -2,12 +2,14 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MainClass extends ApplicationAdapter {
+public class MainClass extends ApplicationAdapter implements InputProcessor{
 	SpriteBatch batch;
 	Texture img;
 	Sprite sprite;
@@ -17,6 +19,8 @@ public class MainClass extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		sprite = new Sprite(img);
+		
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -24,7 +28,59 @@ public class MainClass extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(sprite, 50, 80);
+		batch.draw(sprite, sprite.getX(), sprite.getY());
 		batch.end();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		if(keycode == Input.Keys.LEFT){
+			sprite.translateX(-1f);
+		} else if (keycode == Input.Keys.RIGHT){
+			sprite.translateX(1f);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
